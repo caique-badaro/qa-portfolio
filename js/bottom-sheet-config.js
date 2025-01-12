b7e0e3142aa85141e9797874f53793ce783c3d23
caique-badaro <142758845+caique-badaro@users.noreply.github.com>
2025-01-11 21:49:45 -0300

    Add files via upload

--- js/bottom-sheet-config.js
+++ js/bottom-sheet-config.js
@@ -1,60 +1,11 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const button = document.getElementById("bottom-sheet-01");
-//     const popup = document.getElementById("cb_bottom-sheet--block-content");
-//     const overlay = document.getElementById("cb_overlay");
-
-//     // Abre o popup e overlay
-//     button.addEventListener("click", function () {
-//       popup.style.display = "block";
-//       overlay.style.display = "block";
-//     });
-
-//     // Fecha o popup ao clicar nele
-//     popup.addEventListener("click", function () {
-//       popup.style.display = "none";
-//       overlay.style.display = "none";
-//     });
-
-//     // Adiciona a funcionalidade de fechamento ao overlay também, se necessário
-//     overlay.addEventListener("click", function () {
-//       popup.style.display = "none";
-//       overlay.style.display = "none";
-//     });
-//   });
-
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     const button1 = document.getElementById("bottom-sheet-01");
-//     const button2 = document.getElementById("bottom-sheet-02");
-//     const popup = document.getElementById("cb_bottom-sheet--block-content");
-//     const overlay = document.getElementById("cb_overlay");
-
-//     // Função para abrir o popup e overlay
-//     function openPopup() {
-//         popup.style.display = "block";
-//         overlay.style.display = "block";
-//     }
-
-//     // Função para fechar o popup e overlay
-//     function closePopup() {
-//         popup.style.display = "none";
-//         overlay.style.display = "none";
-//     }
-
-//     // Adiciona evento de clique para abrir o popup nos botões
-//     button1.addEventListener("click", openPopup);
-//     button2.addEventListener("click", openPopup);
-
-//     // Fecha o popup ao clicar nele
-//     popup.addEventListener("click", closePopup);
-
-//     // Fecha o popup ao clicar no overlay
-//     overlay.addEventListener("click", closePopup);
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
+  // card heatmap mouse move
   const button1 = document.getElementById("bottom-sheet-01");
+  // card heatmap clicks
   const button2 = document.getElementById("bottom-sheet-02");
+  // card heatmap scroll
+  const button3 = document.getElementById("bottom-sheet-03");
+
   const popup = document.getElementById("cb_bottom-sheet--block-content");
   const overlay = document.getElementById("cb_overlay");
   const contentDiv = document.getElementById("content-bottom-sheet");
@@ -71,16 +22,23 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   }
 
-  // Abre o popup e overlay e carrega o conteúdo do botão 1
+  // Abre o popup e overlay e carrega o conteúdo do botão 1 = heatmap mouse move
   button1.addEventListener("click", function () {
-    loadContent("content/bottom-sheet_modal-teste.html");
+    loadContent("content/bottom-sheet_modal_heatmap-move-mouse.html");
     popup.style.display = "block";
     overlay.style.display = "block";
   });
 
-  // Abre o popup e overlay e carrega o conteúdo do botão 2
+  // Abre o popup e overlay e carrega o conteúdo do botão 2 = heatmap clicks
   button2.addEventListener("click", function () {
-    loadContent("content/bottom-sheet_modal-reduced.html");
+    loadContent("content/bottom-sheet_modal_heatmap-move-mouse.html");
+    popup.style.display = "block";
+    overlay.style.display = "block";
+  });
+
+  // Abre o popup e overlay e carrega o conteúdo do botão 3 = heatmap scroll
+  button3.addEventListener("click", function () {
+    loadContent("content/bottom-sheet_modal_heatmap-scroll.html");
     popup.style.display = "block";
     overlay.style.display = "block";
   });
