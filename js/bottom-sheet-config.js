8c33efc1f8f6c8fadf89912747ab722c9c6d091d
caique-badaro <142758845+caique-badaro@users.noreply.github.com>
2025-01-16 21:32:05 -0300

    Add files via upload

--- js/bottom-sheet-config.js
+++ js/bottom-sheet-config.js
@@ -5,12 +5,15 @@ document.addEventListener("DOMContentLoaded", function () {
     { id: "bottom-sheet-03", content: "content/revisao-ux-ui-apoio-entrega/bottom-sheet_modal_heatmap-scroll.html" },
     { id: "bottom-sheet-04", content: "content/revisao-ux-ui-apoio-entrega/bottom-sheet_modal_pesquisa-matriz-csd.html" },
     { id: "bottom-sheet-05", content: "content/revisao-ux-ui-apoio-entrega/bottom-sheet_modal_pesquisa-hipoteses.html" },
-    { id: "bottom-sheet-06", content: "content/revisao-ux-ui-apoio-entrega/bottom-sheet_modal_pesquisa-pesquisa-formulario.html" }
+    { id: "bottom-sheet-06", content: "content/revisao-ux-ui-apoio-entrega/bottom-sheet_modal_pesquisa-pesquisa-formulario.html" },
+    { id: "bottom-sheet-07", content: "content/revisao-ux-ui-apoio-entrega/bottom-sheet_modal_project-prototype-mobile.html"},
+    { id: "bottom-sheet-08", content: "content/revisao-ux-ui-apoio-entrega/bottom-sheet_modal_project-prototype-desktop.html"}
   ];
 
   const popup = document.getElementById("cb_bottom-sheet--block-content");
   const overlay = document.getElementById("cb_overlay");
   const contentDiv = document.getElementById("content-bottom-sheet");
+  const closeButton = document.getElementById("cb_btn-icon-close--bottom-sheet");
 
   // Função para carregar conteúdo HTML no popup
   function loadContent(url) {
@@ -38,16 +41,19 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  // Fecha o popup ao clicar nele
-  popup.addEventListener("click", function () {
+  // Função para fechar o popup
+  function closePopup() {
     popup.style.display = "none";
     overlay.style.display = "none";
-  });
+  }
+
+  // Fecha o popup ao clicar no botão de fechar
+  if (closeButton) {
+    closeButton.addEventListener("click", closePopup);
+  }
 
   // Fecha o popup ao clicar no overlay
-  overlay.addEventListener("click", function () {
-    popup.style.display = "none";
-    overlay.style.display = "none";
-  });
+  if (overlay) {
+    overlay.addEventListener("click", closePopup);
+  }
 });
-
